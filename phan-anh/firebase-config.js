d32e9618b14ca77a5cc56cb518e4bb738612a57d
Meicit <it.trancongminh@gmail.com>
2026-06-11 17:21:40 +0700

    Update firebase-config.js

--- phan-anh/firebase-config.js
+++ phan-anh/firebase-config.js
@@ -3,14 +3,16 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/fireba
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-storage.js";
 
-// ĐỒNG BỘ: Thay thế các chuỗi ký tự dưới đây bằng thông số dự án của bạn
+// ĐỒNG BỘ ĐẦY ĐỦ VÀ CHUẨN HÓA THEO THÔNG SỐ DỰ ÁN CỦA BẠN
 const firebaseConfig = {
     apiKey: "AIzaSyCjS8jJtoV5HLcYIpVTX1zP5DbyYmXsbh4",
     authDomain: "dashboard-demo-ce0da.firebaseapp.com",
+    databaseURL: "https://dashboard-demo-ce0da-default-rtdb.asia-southeast1.firebasedatabase.app",
     projectId: "dashboard-demo-ce0da",
-    storageBucket: "dashboard-demo-ce0da.firebasestorage.app",
+    storageBucket: "dashboard-demo-ce0da.appspot.com", // 🌟 ĐÃ SỬA THÀNH APPSPOT.COM ĐỂ TRÁNH LỖI KẾT NỐI STORAGE
     messagingSenderId: "969593884637",
-    appId: "1:969593884637:web:47d4e6b03a51e7dcfbb034"
+    appId: "1:969593884637:web:47d4e6b03a51e7dcfbb034",
+    measurementId: "G-EVMDSWWRFG"
 };
 
 // Khởi tạo thực thể ứng dụng Firebase
